d0e69e5edf99a7be109089a93f06be96f82ec551
clickomat <sconnell306@gmail.com>
2018-05-07 14:51:34 -0600

    Changes as discussed in slack

--- public/js/finder.js
+++ public/js/finder.js
@@ -1,6 +1,6 @@
-// Test for finder.js linked to finder.html
-var para = document.createElement("p");
-var node = document.createTextNode("finder js is linked.");
-para.appendChild(node);
-var element = document.getElementById("JS");
-element.appendChild(para);
+// // Test for finder.js linked to finder.html
+// var para = document.createElement("p");
+// var node = document.createTextNode("finder js is linked.");
+// para.appendChild(node);
+// var element = document.getElementById("JS");
+// element.appendChild(para);
