6767c54386a03f04bfb7079c4e1376ec86faa9d4
clickomat <sconnell306@gmail.com>
2018-05-09 20:35:35 -0600

    added an unfinished materalize button to the omega page and put the firebase back

---
+++ public/js/finder.js
@@ -0,0 +1,8 @@
+window.onload=function(){
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left'
+    });
+  });
+};
