c2a5a45d68b78168478ba355ab550ef06e1719cb
chad green <chadgreen@chads-mbp.uconnect.utah.edu>
2018-05-12 10:26:51 -0600

    infoWindow layout

--- public/js/finder.js
+++ public/js/finder.js
@@ -1,10 +1,15 @@
-
-var map, infoWindow;
+var map, infoWindow, infoWindowo;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 40.5696112, lng: -111.8955148 },
         zoom: 19
     });
+    let autocomplete = new google.maps.places.Autocomplete();
+
+    autocomplete.bindTo('bounds', map);
+    autocomplete.addListener('place_changed', () => {
+        const place = autocomplete.getPlace();
+    })
 
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
@@ -18,15 +23,15 @@ function initMap() {
                 position: pos,
                 title: 'current location',
                 icon: '../images/yourloc.png'
-                 
+
             });
-            
+
 
             //infoWindow.setPosition(pos);
             //infoWindow.setContent('Location found.');
             //infoWindow.open(map);
             map.setCenter(pos);
-// google places search to find Bathrroms and plotting them on the map            
+            // google places search to find Bathrroms and plotting them on the map            
             var service = new google.maps.places.PlacesService(map);
             service.nearbySearch({
                 location: pos,
@@ -50,12 +55,33 @@ function initMap() {
                 position: place.geometry.location,
                 icon: '../images/maptoilet.svg'
             });
-            var infoWindow = new google.maps.InfoWindow({
-                content: '<h1>Whatever we want</h1>' 
+            var contentString =
+                '<link rel="stylesheet" href="css/style.css">' +
+                '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">' +
+                '<h1 id="firstHeading" class="firstHeading">Omega Review</h1>' +
+                '<h3>'+ place.name + '</h3>' +
+                '<span class="fa fa-star checked"></span>' +
+                '<span class="fa fa-star checked"></span>' +
+                '<span class="fa fa-star checked"></span>' +
+                '<span class="fa fa-star"></span>' +
+                '<span class="fa fa-star"></span>' +
+                '<br>' +
+                '<br>' +
+                '<img src="../images/publicbathroom.jpeg">' +
+                '<div id="dmap"></div>' +
+                place.id +
+                '<button>Rate</button>' +
+                '<button>Add a Photo</button>';
+            const infoWindowo = new google.maps.InfoWindow({
+                content: contentString
             });
             marker.addListener('click', function () {
-                infoWindow.open(map, marker);
+                infoWindowo.open(map, marker);
+                infoWindowo.addListener('directions', 'click', function () {
+                    infoWindowo.colculateAndRenderDirections(pos, place.geometry.location);
+                });
             });
+
         }
     } function handleLocationError() {
         handleLocationError(true, infoWindow, map.getCenter());
@@ -73,17 +99,18 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 const colculateAndRenderDirections = (origin, destination) => {
     let directionsService = new google.maps.DirectionsService(),
         directionsDisplay = new googl.maps.DirectionRenderer(),
-        request = { 
+        request = {
             origin: origin,
             destination: destination,
             travelMode: 'WALKING'
         }
-    directionsDisplay.setMap(map);
+    directionsDisplay.setMap(dmap);
     directionsService.route(request, (result, status) => {
         if (status == 'ok') {
             directionsDisplay.setDirections(result);
         }
     })
 }
+
 
 
