859610f8995855d9a682eba1a2517483ba488b28
clickomat <sconnell306@gmail.com>
2018-05-15 18:59:10 -0600

    some changes please ignore api changes for now

--- public/js/omega.js
+++ public/js/omega.js
@@ -1,3 +1,3 @@
 
 // fades out after 3 seconds and redirects to the map
-<script>setTimeout(function(){window.location.href="../public/finder.html"},3000);</script>
+setTimeout(function(){window.location.href="../public/finder.html"},3000);
